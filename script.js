a7ab2522a1cbdcd142393ba50834bfc077955e02
yarbie-1 <153131478+yarbie-1@users.noreply.github.com>
2023-12-06 14:51:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const board = document.getElementById('game-board');
 const boardSize = 10; // 10x10 grid
 let pacManPosition = 0;
+let score = 0;
+const walls = [1, 2, 4, 7, 8]; // Array of wall positions
+let ghostPositions = [3, 6]; // Starting positions of ghosts
 
 // Create Grid
 function createGrid() {
@@ -14,6 +17,37 @@ function createGrid() {
             square.classList.add('dot');
         }
     }
+}
+
+// Adding Walls and Ghosts
+function addWallsAndGhosts() {
+    const squares = document.querySelectorAll('.grid-square');
+    walls.forEach(index => squares[index].classList.add('wall'));
+    ghostPositions.forEach(index => squares[index].classList.add('ghost'));
+}
+
+// Check for Dot
+function eatDot(square) {
+    if (square.classList.contains('dot')) {
+        square.classList.remove('dot');
+        score += 10;
+        // Update score display if you have one
+    }
+}
+
+// Move Ghosts
+function moveGhosts() {
+    const squares = document.querySelectorAll('.grid-square');
+    ghostPositions.forEach((pos, index) => {
+        squares[pos].classList.remove('ghost');
+        // Simple random movement
+        let move = Math.floor(Math.random() * 4);
+        if (move === 0 && pos % boardSize !== 0) ghostPositions[index] -= 1;
+        if (move === 1 && pos - boardSize >= 0) ghostPositions[index] -= boardSize;
+        if (move === 2 && pos % boardSize < boardSize - 1) ghostPositions[index] += 1;
+        if (move === 3 && pos + boardSize < boardSize * boardSize) ghostPositions[index] += boardSize;
+        squares[ghostPositions[index]].classList.add('ghost');
+    });
 }
 
 // Move Pac-Man
@@ -36,6 +70,13 @@ function movePacMan(e) {
 
     squares[pacManPosition].classList.add('pac-man');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    createGrid();
+    addWallsAndGhosts();
+    document.addEventListener('keyup', movePacMan);
+    setInterval(moveGhosts, 1000); // Move ghosts every second
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     createGrid();
