d0caf7af7e090f028b4d0a0e8f01a6cd3c996bf2
yarbie-1 <153131478+yarbie-1@users.noreply.github.com>
2023-12-06 15:06:56 -0600

    Update createBoard.js

--- createBoard.js
+++ createBoard.js
@@ -1,1 +1,15 @@
 
+
+Create Grid
+function createGrid() {
+    for (let i = 0; i < boardSize * boardSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        board.appendChild(square);
+
+        // Add dots
+        if (Math.random() > 0.8) {
+            square.classList.add('dot');
+        }
+    }
+}
